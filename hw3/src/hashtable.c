81a13fad43f52069f8a1982329ec16e94df9c18b
Nurlan Tulemisov <nurlan.tulemisov@gmail.com>
2024-01-28 17:18:30 +0300

    hw3: hashmap

--- hw3/src/hashtable.c
+++ hw3/src/hashtable.c
@@ -3,63 +3,70 @@
 #include <strings.h>
 #include <stdio.h>
 
-typedef struct {
-    char* key;
+typedef struct
+{
+    char *key;
     int32_t v;
 } bucket;
 
-typedef struct {
+typedef struct
+{
     size_t size;
     size_t cap;
-    bucket* values;
+    bucket *values;
     uint32_t collision;
 } HTable;
 
 /**
  * Выделяет память и инициализирует новую хэш-таблицу.
- * 
+ *
  * @param sz Размер хэш-таблицы.
  * @param collision Параметр столкновения, определяющий максимальное количество коллизий при добавлении элементов.
  * @return Указатель на новую хэш-таблицу, или NULL в случае ошибки выделения памяти.
  */
-HTable* new_table(size_t sz, uint32_t collision)
-{ 
-  HTable* p = malloc(sizeof(HTable));
-  p->size = sz;
-  p->cap = 0;
-  p->collision = collision;
-  bucket* ptr = (bucket*) malloc(sz * sizeof(bucket));
-  if (!ptr) {
-    return NULL;
-  }
-  memset(ptr, 0, sz * sizeof(bucket));
-  p->values = ptr;
-  return p;
+HTable *new_table(size_t sz, uint32_t collision)
+{
+    HTable *p = malloc(sizeof(HTable));
+    p->size = sz;
+    p->cap = 0;
+    p->collision = collision;
+    bucket *ptr = (bucket *)malloc(sz * sizeof(bucket));
+    if (!ptr)
+    {
+        return NULL;
+    }
+    memset(ptr, 0, sz * sizeof(bucket));
+    p->values = ptr;
+    return p;
 }
 
-size_t hash(HTable* t, char* key)
+size_t hash(HTable *t, char *key)
 {
     uint32_t h = 0;
     size_t keyLength = strlen(key);
-    for (size_t i = 0; i < keyLength; i++) {
+    for (size_t i = 0; i < keyLength; i++)
+    {
         h = h * 31 + key[i];
     }
     return h % t->size;
 }
 
-bool grow_bucket_size(HTable* t)
+bool grow_bucket_size(HTable *t)
 {
     size_t old_size = t->size;
-    bucket* oldBuckets = t->values;
+    bucket *oldBuckets = t->values;
     t->size *= 2;
-    bucket* ptr = (bucket*) malloc(t->size * sizeof(bucket));
-    if (!ptr) {
+    bucket *ptr = (bucket *)malloc(t->size * sizeof(bucket));
+    if (!ptr)
+    {
         return false;
     }
     memset(ptr, 0, t->size * sizeof(bucket));
-    
-    for (size_t i = 0; i < old_size; i++) {
-        if (oldBuckets[i].key != NULL) {
+
+    for (size_t i = 0; i < old_size; i++)
+    {
+        if (oldBuckets[i].key != NULL)
+        {
             size_t idx = hash(t, oldBuckets[i].key);
             ptr[idx].key = oldBuckets[i].key;
             ptr[idx].v = oldBuckets[i].v;
@@ -73,30 +80,35 @@ bool grow_bucket_size(HTable* t)
     return true;
 }
 
-int32_t get(HTable* t, char* key)
+int32_t get(HTable *t, char *key)
 {
     size_t index = hash(t, key);
     size_t i = 0;
 
-    for (i = 0; i < t->collision; i++) {
-        if (t->values[index].key != NULL && strncmp(t->values[index].key, key, strlen(key)) == 0) {
+    for (i = 0; i < t->collision; i++)
+    {
+        if (t->values[index].key != NULL && strncmp(t->values[index].key, key, strlen(key)) == 0)
+        {
             return t->values[index].v;
         }
-		index = (index + 1) % t->size;
-	}
+        index = (index + 1) % t->size;
+    }
 
     return -1;
 }
 
-bool insert(HTable* t, char* key, int32_t value, size_t index)
+bool insert(HTable *t, char *key, int32_t value, size_t index)
 {
     size_t i = 0;
 
-    for (i = 0; i < t->collision; i++) {
-        if (t->values[index].key == NULL) {
+    for (i = 0; i < t->collision; i++)
+    {
+        if (t->values[index].key == NULL)
+        {
             t->values[index].v = value;
-            char* ptr = (char*)malloc((strlen(key) + 1) * sizeof(char));
-            if (!ptr) {
+            char *ptr = (char *)malloc((strlen(key) + 1) * sizeof(char));
+            if (!ptr)
+            {
                 printf("cant alloc %s\n", key);
                 return false;
             }
@@ -106,71 +118,74 @@ bool insert(HTable* t, char* key, int32_t value, size_t index)
 
             return true;
         }
-		index = (index + 1) % t->size;
-	}
+        index = (index + 1) % t->size;
+    }
 
-	return false;
+    return false;
 }
 
-
-bool replace_value(HTable* t, char* key, int32_t value, size_t index)
+bool replace_value(HTable *t, char *key, int32_t value, size_t index)
 {
     size_t i = 0;
 
-    for (i = 0; i < t->collision; i++) {
-        if (t->values[index].key != NULL && strncmp(t->values[index].key, key, strlen(key)) == 0) {
+    for (i = 0; i < t->collision; i++)
+    {
+        if (t->values[index].key != NULL && strncmp(t->values[index].key, key, strlen(key)) == 0)
+        {
             t->values[index].v = value;
 
             return true;
         }
-		index = (index + 1) % t->size;
-	}
+        index = (index + 1) % t->size;
+    }
 
-	return false;
+    return false;
 }
-
 
 /**
  * Добавляет элемент или заменяет значение в хэш-таблице.
- * 
+ *
  * @param t Указатель на хэш-таблицу.
  * @param key Ключ элемента.
  * @param value Значение элемента.
  * @return true, если элемент был успешно добавлен, и false, если добавление не удалось.
- * 
+ *
  * Если при добавлении элемента количество элементов в хэш-таблице превышает выделенную память,
  * происходит перераспределение памяти с помощью функции grow_bucket_size.
  * При этом выделяется новый блок памяти большего размера, копируются все существующие элементы в новый блок памяти,
  * а старый блок памяти освобождается.
  */
-bool put(HTable* t, char* key, int32_t value)
+bool put(HTable *t, char *key, int32_t value)
 {
     size_t index = hash(t, key);
     int32_t old_v = get(t, key);
 
-    if (old_v != -1) {
+    if (old_v != -1)
+    {
         return replace_value(t, key, value, index);
     }
 
-    if (t->cap+1 > t->size && !grow_bucket_size(t)) {
+    if (t->cap + 1 > t->size && !grow_bucket_size(t))
+    {
         return false;
     }
 
     return insert(t, key, value, index);
 }
 
-void iterate(HTable* t, void (*callback)(char*, int32_t)) {
-    for (size_t i = 0; i < t->size; i++) {
-        if (t->values[i].key != NULL) {
+void iterate(HTable *t, void (*callback)(char *, int32_t))
+{
+    for (size_t i = 0; i < t->size; i++)
+    {
+        if (t->values[i].key != NULL)
+        {
             callback(t->values[i].key, t->values[i].v);
         }
     }
 }
 
-void clear(HTable* t)
+void clear(HTable *t)
 {
     free(t->values);
     free(t);
 }
-
-
--- hw3/src/main.c
+++ hw3/src/main.c
@@ -3,69 +3,80 @@
 #include <string.h>
 #include "hashtable.h"
 
-void printCallback(char* key, int32_t value) {
+void printCallback(char *key, int32_t value)
+{
     printf("Key: %s, Value: %d\n", key, value);
 }
 
-void print_help(void) {
+void print_help(void)
+{
     printf("Usage: ./myprog -i <input_path> -o <output_path> -c <charset>\n");
     printf("Options:\n");
     printf("  -i <input_path>  Input file path\n");
     printf("  -h               Print this help message\n");
 }
 
-int main(int argc, char* argv[]) 
+int main(int argc, char *argv[])
 {
     char *pin = NULL;
 
     int c;
-    while ((c = getopt(argc, argv, "i:h")) != -1) {
-        switch (c) {
-            case 'i':
-                pin = optarg;
-                break;
-            case 'h':
-                print_help();
-                return 0;
-            default:
-                printf("Invalid option: %c\n", c);
-                return 1;
+    while ((c = getopt(argc, argv, "i:h")) != -1)
+    {
+        switch (c)
+        {
+        case 'i':
+            pin = optarg;
+            break;
+        case 'h':
+            print_help();
+            return 0;
+        default:
+            printf("Invalid option: %c\n", c);
+            return 1;
         }
     }
 
-    if (pin == NULL) {
+    if (pin == NULL)
+    {
         printf("Missing required options: -i, -o, -c\n");
         return EXIT_FAILURE;
     }
 
-    
     FILE *f = fopen(pin, "rb");
-    if (f == NULL) {
+    if (f == NULL)
+    {
         printf("Failed open file");
         return EXIT_FAILURE;
     }
 
-    HTable* t = new_table(10, 10);
-    if (t == NULL) {
+    HTable *t = new_table(10, 10);
+    if (t == NULL)
+    {
         printf("Failed init hashmap");
         return EXIT_FAILURE;
     }
 
     int ch;
     size_t str_len = 0;
-    while ((ch = fgetc(f)) != EOF) {
-        if (ch == ' ' || ch == '.' || ch == ',' || ch == '\n' || ch == '\t') {
-            if (str_len != 0) {
-                fseek(f, -str_len-1, SEEK_CUR);
-                char* word = (char*)malloc((str_len + 1) * sizeof(char));
+    while ((ch = fgetc(f)) != EOF)
+    {
+        if (ch == ' ' || ch == '.' || ch == ',' || ch == '\n' || ch == '\t')
+        {
+            if (str_len != 0)
+            {
+                fseek(f, -str_len - 1, SEEK_CUR);
+                char *word = (char *)malloc((str_len + 1) * sizeof(char));
                 fread(word, sizeof(char), str_len, f);
                 word[str_len] = '\0'; // Добавляем завершающий нулевой символ
 
                 int32_t v = get(t, word);
-                if (v == -1) {
+                if (v == -1)
+                {
                     v = 0;
                 }
-                if (!put(t, word, ++v)) {
+                if (!put(t, word, ++v))
+                {
                     printf("Could not add. key: %s => value: %d\n", word, v);
                     return EXIT_FAILURE;
                 }
