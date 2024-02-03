ff0a06e507ea47ac53de680ed1faca8ef5acc7da
Nurlan Tulemisov <nurlan.tulemisov@gmail.com>
2024-02-04 00:30:54 +0300

    hw3 cmakefile

---
+++ hw3/build/CMakeFiles/myprogram.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myprogram.dir/src/hashtable.c.o"
+  "CMakeFiles/myprogram.dir/src/hashtable.c.o.d"
+  "CMakeFiles/myprogram.dir/src/main.c.o"
+  "CMakeFiles/myprogram.dir/src/main.c.o.d"
+  "myprogram"
+  "myprogram.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/myprogram.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
